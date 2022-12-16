78af544792eb7dbb9a789ee17a8e2a2eb8583401
cph101 <cph101@icloud.com>
2022-12-16 15:08:20 +0000

    Create Set weight.js

---
+++ Fishing Frenzy/Set weight.js
@@ -0,0 +1,20 @@
+(async () => {
+    let n = document.createElement('iframe');
+    document.body.append(n);
+    window.alert = n.contentWindow.alert.bind(window);
+    window.prompt = n.contentWindow.prompt.bind(window);
+    window.confirm = n.contentWindow.confirm.bind(window);
+    n.remove();
+            function reactHandler() {
+                return Object.values(document.querySelector('#app > div > div'))[1].children[1]._owner;
+            };
+        
+            if (window.location.pathname != '/play/fishing') {
+                alert('You must be in a fishing frenzy game!');
+            } else {
+                let weight = Number(parseFloat(prompt('How much weight do you want?')));
+                reactHandler().stateNode.setState({ weight2: weight, weight });
+        
+                alert('Weight set!');
+            };
+})();
