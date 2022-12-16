fc6b352b94ec5b1815588c5d70ae39872c0f596b
cph101 <cph101@icloud.com>
2022-12-16 15:11:50 +0000

    Create Max stats.js

---
+++ Crazy Kingdom/Max stats.js
@@ -0,0 +1,19 @@
+(async () => {
+    let n = document.createElement('iframe');
+    document.body.append(n);
+    window.alert = n.contentWindow.alert.bind(window);
+    window.prompt = n.contentWindow.prompt.bind(window);
+    window.confirm = n.contentWindow.confirm.bind(window);
+    n.remove();
+            function reactHandler() {
+                return Object.values(document.querySelector('#app > div > div'))[1].children[1]._owner;
+            };
+        
+            if (window.location.pathname != '/kingdom') {
+                alert('You must be in a crazy kingdom game!');
+            } else {
+                reactHandler().stateNode.setState({ materials: 100, people: 100, happiness: 100, gold: 100 });
+        
+                alert('Max stats set!');
+            };
+})();
