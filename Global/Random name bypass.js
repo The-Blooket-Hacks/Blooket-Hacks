80c710a0f637894722a600400e78fa915613c05d
cph101 <cph101@icloud.com>
2022-12-16 14:43:37 +0000

    Create Random name bypass.js

---
+++ Global/Random name bypass.js
@@ -0,0 +1,5 @@
+var obj = Object.values(document.querySelector('#app > div > div'))[1].children[1]['_owner'].stateNode.state;
+if (obj.isRandom = true) {
+  obj.isRandom = false
+}
+document.getElementsByClassName("styles__randomButton___3CTp0-camelCase")[0].click()
