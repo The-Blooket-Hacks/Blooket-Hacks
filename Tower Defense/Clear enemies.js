48581afea6b6b74e0b2a49e1b7734d15fac85b99
cph101 <cph101@icloud.com>
2022-12-16 15:15:30 +0000

    Create Clear enemies.js

---
+++ Tower Defense/Clear enemies.js
@@ -0,0 +1,19 @@
+(async () => {
+    let n = document.createElement('iframe');
+    document.body.append(n);
+    window.alert = n.contentWindow.alert.bind(window);
+    window.prompt = n.contentWindow.prompt.bind(window);
+    window.confirm = n.contentWindow.confirm.bind(window);
+    n.remove();
+            function reactHandler() {
+                return Object.values(document.querySelector('#app > div > div'))[1].children[1]._owner;
+            };
+
+            if (window.location.pathname != '/defense') {
+                alert('You must be in a tower defense game!');
+            } else {
+                reactHandler().stateNode.enemies = [];
+
+                alert('Enemies cleared!');
+            };
+})();
