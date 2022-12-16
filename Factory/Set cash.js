1843b7c74ce635fd08025b1e335883ceda7b896f
cph101 <cph101@icloud.com>
2022-12-16 15:20:55 +0000

    Create Set cash.js

---
+++ Factory/Set cash.js
@@ -0,0 +1,8 @@
+var obj = Object.values(document.querySelector("#app > div > div"))[1].children[1]._owner.stateNode.state;
+let a = document.createElement("iframe");
+document.body.append(a);
+window.prompt = a.contentWindow.prompt.bind(window);
+a.style.frameBorder="none"
+a.remove();
+var currencyInput = prompt("How much money?")
+obj.cash = currencyInput;
