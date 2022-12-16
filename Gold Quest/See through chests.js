c32f30b37b1f5bc836609a0a82868aae0ef957ca
cph101 <cph101@icloud.com>
2022-12-16 14:53:03 +0000

    Create See through chests.js

---
+++ Gold Quest/See through chests.js
@@ -0,0 +1,53 @@
+(async () => {
+    let n = document.createElement('iframe');
+    document.body.append(n);
+    window.alert = n.contentWindow.alert.bind(window);
+    window.prompt = n.contentWindow.prompt.bind(window);
+    window.confirm = n.contentWindow.confirm.bind(window);
+    n.remove();
+            if (window.location.pathname != '/play/gold') {
+                alert('You must be in a gold quest game!');
+            } else {
+                alert('Chest ESP enabled');
+
+                interval = setInterval(() => {
+                    if (window.location.pathname != '/play/gold') return clearInterval(interval);
+
+                    data = Object.values(document.querySelector('#app > div > div'))[1].children[1]._owner.stateNode.state;
+
+                    if (data?.stage == 'prize') {
+                        boxes = data.choices;
+                        choiceDiv = document.querySelector("div[class*='arts__regularBody']")?.children[1];
+                        if (!choiceDiv) return clearInterval(interval)
+                        if (!document.querySelector("p[class*='chest-esp']")) boxes.forEach((box, i) => {
+                            textElement = document.createElement('p');
+                            textElement.className = "chest-esp";
+                            textElement.innerText = box.text;
+                            textElement.style = `text-align: center;
+                                font-size: 30px;
+                                color: white;
+                                font-family:Titan One;
+                                sans-serif;
+                                -webkit-user-select:none;
+                                -moz-user-select:none;
+                                -ms-user-select:none;
+                                user-select:none;
+                                border-color: black;
+                                margin-top: 200px;`
+                            try { choiceDiv.children[i].appendChild(textElement); } catch (e) {
+                                if (confirm('An error occured, would you like to report this in the support discord server?')) {
+                                    window.open('https://glizzers.xyz/discord');
+                                };
+                            };
+                        });
+
+                        else boxes.forEach((box, i) => {
+                            if (choiceDiv.children.length == 3 && choiceDiv.children[i].children[1].innerText != box.text) choiceDiv.children[i].children[1].innerText = box.text;
+                        });
+                    };
+
+                    if (window.location.pathname != '/play/gold') clearInterval(interval)
+
+                }, 0);
+            };
+})();
