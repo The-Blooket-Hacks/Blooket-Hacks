939f419f39faab7efd9d70a26e312d88c3fe504d
cph101 <cph101@icloud.com>
2022-12-16 18:31:16 +0000

    Note: slightly broken

--- Global/Higlight Answers.js
+++ Global/Higlight Answers.js
@@ -1,8 +1,11 @@
 function highlightAns(answer) {
     document.getElementById(`answer${Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.question.answers.indexOf(answer)}`).children[0].style = "background-color: rgb(12, 176, 74);";
 }
-document.getElementById("answer0").children[0].style = "color: rgb(255, 255, 255);";
-document.getElementById("answer1").children[0].style = "color: rgb(255, 255, 255);";
-document.getElementById("answer2").children[0].style = "color: rgb(255, 255, 255);";
-document.getElementById("answer3").children[0].style = "color: rgb(255, 255, 255);";
+function highlightAnswers() {
+document.getElementById("answer0").children[0].style = "background-color: rgb(242, 57, 65);";
+document.getElementById("answer1").children[0].style = "background-color: rgb(242, 57, 65);";
+document.getElementById("answer2").children[0].style = "background-color: rgb(242, 57, 65);";
+document.getElementById("answer3").children[0].style = "background-color: rgb(242, 57, 65);";
 Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.question.correctAnswers.forEach(answer => highlightAns(answer))
+}
+window.setInterval(highlightAnswers)
