7a511076c7b3729de86b6d3ebe71d11f741c305b
cph101 <cph101@icloud.com>
2022-12-17 13:43:10 +0000

    Update OP fossils.js

--- Deceptive Dinos/OP fossils.js
+++ Deceptive Dinos/OP fossils.js
@@ -1,1 +1,1 @@
-Object.values(document.querySelector('#app > div > div'))[1].children[0]._owner.stateNode.state.fossilMult = 1000000
+Object.values(document.querySelector('#app > div > div'))[1].children[0]._owner.stateNode.setState({ "fossilMult":10000000000})
