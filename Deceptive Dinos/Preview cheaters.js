b9f962bd7b30fc74ae2c9a52c2bd626dd7b69752
cph101 <cph101@icloud.com>
2022-12-17 13:28:30 +0000

    Create Preview cheaters.js

---
+++ Deceptive Dinos/Preview cheaters.js
@@ -0,0 +1,30 @@
+function checkPlayer(player) {
+    var players = document.getElementsByTagName("div");
+    if (player.isCheating == true) {
+        for(var i=0;i<players.length;i++){
+
+if(players[i].innerHTML == player.name){
+
+players[i].parentNode.parentNode.style = "color:red;";
+break;
+
+}
+    }
+} else {
+        for(var i=0;i<players.length;i++){
+
+if(players[i].innerHTML == player.name){
+
+players[i].parentNode.parentNode.style = "color:green;";
+break;
+
+}
+    }
+        
+    }
+}
+function checkPlayers() {
+Object.values(document.querySelector('#app > div > div'))[1].children[0]._owner.stateNode.state.players.forEach(player => checkPlayer(player))
+}
+
+window.setInterval(checkPlayers)
