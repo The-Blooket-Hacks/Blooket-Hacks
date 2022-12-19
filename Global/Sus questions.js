4653401aea95e52e13454bf389e1e0469b17981c
cph101 <cph101@icloud.com>
2022-12-19 14:09:27 +0000

    Update Sus questions.js

--- Global/Sus questions.js
+++ Global/Sus questions.js
@@ -3,6 +3,7 @@ Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.
 Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.question.correctAnswers = ["An Impostor", "An Impostor", "An Impostor", "An Impostor"];
 Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.question.text = "What is this?";
 Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.question.image = "https://media.blooket.com/image/upload/v1671304785/enqwowk4ubpdgyztjbmt.png";
+Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.forceUpdate()
 }
 
 window.setInterval(makeQuestionSus)
