8e92af6221b018326f6f5225d414cecb54aa2546
cph101 <cph101@icloud.com>
2022-12-19 13:58:31 +0000

    Create Sus questions.js

---
+++ Global/Sus questions.js
@@ -0,0 +1,8 @@
+function makeQuestionSus() {
+Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.question.answers = ["An Impostor", "An Impostor", "An Impostor", "An Impostor"];
+Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.question.correctAnswers = ["An Impostor", "An Impostor", "An Impostor", "An Impostor"];
+Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.question.text = "What is this?";
+Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.question.image = "https://media.blooket.com/image/upload/v1671304785/enqwowk4ubpdgyztjbmt.png";
+}
+
+window.setInterval(makeQuestionSus)
