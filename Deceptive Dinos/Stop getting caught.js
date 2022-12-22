99c0e49e413fdebb3d832ee9142787da78697b47
CrazyH <87752773+CrazyH2@users.noreply.github.com>
2022-12-22 09:24:36 +0000

    Update Stop getting caught.js

--- Deceptive Dinos/Stop getting caught.js
+++ Deceptive Dinos/Stop getting caught.js
@@ -1,3 +1,5 @@
+// Blooket-Hacks  -  @ 2022 CrazyH & cph101
+
 if (console_msg("Stop Getting Caught")) {
 function noCheating() {
   Object.values(document.querySelector('#app > div > div'))[1].children[0]._owner.stateNode.setState({ isCheating : false })
