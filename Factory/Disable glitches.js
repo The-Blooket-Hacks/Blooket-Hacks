7230cfbc1ed6139c66a7bc3594a2f9255a4b109e
CrazyH <87752773+CrazyH2@users.noreply.github.com>
2022-12-22 09:25:08 +0000

    Update Disable glitches.js

--- Factory/Disable glitches.js
+++ Factory/Disable glitches.js
@@ -1,3 +1,5 @@
+// Blooket-Hacks  -  @ 2022 CrazyH & cph101
+
 if (console_msg("Disable Glitches")) {
 function stopGlitch() {
 Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.joke = false
