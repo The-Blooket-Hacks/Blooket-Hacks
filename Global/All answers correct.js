66c66355e0d9c849cefc011d5aaabdef811ef00a
CrazyH <87752773+CrazyH2@users.noreply.github.com>
2022-12-22 09:20:08 +0000

    Update All answers correct.js

--- Global/All answers correct.js
+++ Global/All answers correct.js
@@ -1,3 +1,5 @@
+// Blooket-Hacks  -  @ 2022 CrazyH & cph101
+
 if (console_msg("All Answers Correct")) {
 setInterval(function() {
   var obj = Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.question;
