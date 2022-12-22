d0c673c0f2f797fe967446cd4eceedf0cabb6c65
CrazyH <87752773+CrazyH2@users.noreply.github.com>
2022-12-22 09:24:03 +0000

    Update OP fossils.js

--- Deceptive Dinos/OP fossils.js
+++ Deceptive Dinos/OP fossils.js
@@ -1,3 +1,5 @@
+// Blooket-Hacks  -  @ 2022 CrazyH & cph101
+
 if (console_msg("Op Fossils")) {
 Object.values(document.querySelector('#app > div > div'))[1].children[0]._owner.stateNode.setState({ "fossilMult":10000000000})
 }
