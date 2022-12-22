071436d95238397878eb3ac66526a8a3da41c80c
CrazyH <87752773+CrazyH2@users.noreply.github.com>
2022-12-22 09:20:27 +0000

    Update Anti ban.js

--- Global/Anti ban.js
+++ Global/Anti ban.js
@@ -1,3 +1,5 @@
+// Blooket-Hacks  -  @ 2022 CrazyH & cph101
+
 const original_open = XMLHttpRequest.prototype.open;
 XMLHttpRequest.prototype.open = function () {
     if (!arguments[1].includes("suspend")) original_open.apply(this, arguments)
