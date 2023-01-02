75cdc74f0a1f0ac5ed8da74eb2b3cde19ee79e34
cph101 <cph101@icloud.com>
2023-01-02 15:55:19 +0000

    Make more OP

--- Factory/Always get Spooky Ghost.js
+++ Factory/Always get Spooky Ghost.js
@@ -2,18 +2,25 @@
 
 if (console_msg("Always Get Spooky Ghost")) {
  setInterval(function() {
+try {
    var obj = Object.values(document.querySelector("#app > div > div"))[1].children[1]._owner.stateNode.state.choices[0];
    obj.name = "Spooky Ghost";
    obj.time = [
-     0.1, 0.1 , 0.1 , 0.1 , 0.1];
-   obj.cash = [1e100, 1e100, 1e100, 1e100, 1e100];
+     1, 1 , 1 , 1 , 1];
+   obj.cash = [100000000000000000e100000000, 100000000000000000e100000000, 100000000000000000e100000000, 100000000000000000e100000000, 100000000000000000e100000000];
    obj.price = [0,0,0,0
    ]
    obj.class = "👻";
    obj.rarity = "Impossible";
    obj.level = 4;
    obj.bonus = 1.65
- })
+} catch(error) {
+    console.log("")
+}
+ });
+ document.head.innerHTML += `<style> .styles__factoryBar___dHE0M-camelCase {
+background-color: rgb(102, 245, 145) !important;
+} </style>`;
 }
 
 // Console Message Code
