eeccdc19943bbf49e5dc68ea0eb93b07405069be
cph101 <cph101@icloud.com>
2023-01-02 15:11:06 +0000

    Update and rename No damage.js to Godmode.js

--- Monster Brawl/Godmode.js
+++ Monster Brawl/Godmode.js
@@ -1,3 +1,22 @@
+function reactEventHandler() {
+    let react = Object.values(document.querySelector("#body"))[0].stateNode;
+    return react[Object.keys(react).filter(a => a.includes("reactInternalInstance"))].return.stateNode;
+}
+
+(() => {
+    let n = document.createElement('iframe');
+    document.body.append(n);
+    window.prompt = n.contentWindow.prompt.bind(window);
+    window.alert = n.contentWindow.alert.bind(window);
+    n.remove();
+})();
+damage = 1000000000000000000e10000000000;
+
+dmgSetter = setInterval(() => {
+    reactEventHandler().state.dmgArray.map(a => a.dmg = damage);
+}, 1);
+
+(function(){
 function getStateNode(){
     for(let i of Object.keys(document.querySelector("#app>div>div"))){
         if(i.toString().includes("__reactEventHandlers")){
@@ -31,3 +50,4 @@ if(first){
 }
 }
 window.setInterval(()=>getStateNode().state.game.scene.children.list.filter(x=>x.dmg&&x.hp).map(e=>e.dmg=0));
+})();
