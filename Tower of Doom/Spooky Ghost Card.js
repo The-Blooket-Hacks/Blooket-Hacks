ca13e25f01c28e02bdfa48247c43ba37ba8f3b86
cph101 <cph101@icloud.com>
2023-01-03 13:51:57 +0000

    Create Spooky Ghost Card.js

---
+++ Tower of Doom/Spooky Ghost Card.js
@@ -0,0 +1,1 @@
+window.setInterval(function(){ Object.values(document.querySelector('#app > div > div'))[1].children[1]._owner.stateNode.state.cards = [{ blook: "Spooky Ghost", charisma: 500, class: "🪦", strength: 500, wisdom: 500}] });
