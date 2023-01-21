c6d1c16da0697707334dca000bf40f5f6eb3b52e
CrazyH <87752773+CrazyH2@users.noreply.github.com>
2023-01-21 08:59:30 +0000

    Update Whisper Answers.js

--- Global/Whisper Answers.js
+++ Global/Whisper Answers.js
@@ -23,7 +23,7 @@ window.setInterval(whisperAnswer);
 // Console Message Code
 function console_msg(file, state) {
    if (!state) {
-       if (window.location.host.contains("blooket.com")) {
+       if (window.location.href.indexOf("blooket.com") > -1 && window.location.host !== "www.blooket.com" && window.location.host !== "id.blooket.com") {
            console_msg(file, true);
            return true;
        } else if (window.location.host === "www.blooket.com" || window.location.host === "id.blooket.com") {
