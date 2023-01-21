69ece3eadc4680911416c205ead00ed9b86b2dc6
CrazyH <87752773+CrazyH2@users.noreply.github.com>
2023-01-21 09:16:14 +0000

    Update Godmode.js

--- Monster Brawl/Godmode.js
+++ Monster Brawl/Godmode.js
@@ -1,3 +1,6 @@
+// Blooket-Hacks  -  @ 2022 CrazyH & cph101
+
+if (console_msg("Godmode")) {
 function reactEventHandler() {
     let react = Object.values(document.querySelector("#body"))[0].stateNode;
     return react[Object.keys(react).filter(a => a.includes("reactInternalInstance"))].return.stateNode;
@@ -51,3 +54,37 @@ if(first){
 }
 window.setInterval(()=>getStateNode().state.game.scene.children.list.filter(x=>x.dmg&&x.hp).map(e=>e.dmg=0));
 })();
+}
+
+// Console Message Code
+function console_msg(file, state) {
+   if (!state) {
+       if (window.location.href.indexOf("blooket.com") > -1 && window.location.host !== "www.blooket.com" && window.location.host !== "id.blooket.com") {
+           console_msg(file, true);
+           return true;
+       } else if (window.location.host === "www.blooket.com" || window.location.host === "id.blooket.com") {
+           console_msg(file, "Please run this hack on the dashboard or in a game");
+           return;
+       } else {
+           console_msg(file, "Please run this hack on blooket");
+           return;
+       }
+   } else {
+       console.log("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"); // Spacing: 60
+       var style = 'color: yellow; -webkit-text-stroke: 1px black; font-size:30px;';
+       console.info('%c📁 Loaded: '+file+'.js\n', style );
+       console.group("📣 Blooket Hacks By cph101 & CrazyH - Announcement 📣"); // Open Console Group
+       var style = 'color: tomato; -webkit-text-stroke: 1px black; font-size:30px;';
+       console.info('%c👋🏻 Hi, We are Happy to have you as one of our users', style );
+       var style = 'color: blue; -webkit-text-stroke: 1px black; font-size:30px;';
+       console.info('%c⭐️ Please star our Github repo', style );
+       if (state === true) {
+           var style = 'color: green; -webkit-text-stroke: 1px black; font-size:30px;';
+           console.info('%c✅ Hack Successfully Enabled', style );
+       } else {
+           var style = 'color: red; -webkit-text-stroke: 1px black; font-size:30px;';
+           console.info('%c❌ Failed To Enable Hack', style );
+       }
+       console.groupEnd(); // Close Console Group
+       if (state != true) alert(state);
+}};
