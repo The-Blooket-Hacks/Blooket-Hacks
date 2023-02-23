83eb8648383ee0ac17c5642bfe78dbc97d13fc94
ThatOneGuy69 <94601497+JaydenTD500@users.noreply.github.com>
2023-02-23 18:59:29 +0800

    Update Spooky Ghost Card.js

--- Tower of Doom/Spooky Ghost Card.js
+++ Tower of Doom/Spooky Ghost Card.js
@@ -2,7 +2,7 @@
 
 if (console_msg("Spooky Ghost Card")) {
 window.setInterval(function(){
-    Object.values(document.querySelector('#app > div > div'))[1].children[1]._owner.stateNode.state.myCard = { blook: "Spooky Ghost", charisma: 500, class: "🪦", strength: 500, wisdom: 500}
+    Object.values(document.querySelector('#app > div > div'))[1].children[0]._owner.stateNode.state.myCard = { blook: "Spooky Ghost", charisma: 500, class: "🪦", strength: 500, wisdom: 500}
 })
 }
 
