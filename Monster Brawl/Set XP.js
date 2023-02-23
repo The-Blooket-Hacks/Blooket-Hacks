cfb4ef10b81d8407d085ff6b8b61ff6e3890ab64
ThatOneGuy69 <94601497+JaydenTD500@users.noreply.github.com>
2023-02-23 18:56:31 +0800

    Update Set XP.js

--- Monster Brawl/Set XP.js
+++ Monster Brawl/Set XP.js
@@ -1,7 +1,7 @@
 // Blooket-Hacks  -  @ 2022 CrazyH & cph101
 
 if (console_msg("Set Xp")) {
-var obj = Object.values(document.querySelector("#app > div > div"))[1].children[1]._owner.stateNode.state;
+var obj = Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state;
 let a = document.createElement("iframe");
 document.body.append(a);
 window.prompt = a.contentWindow.prompt.bind(window);
