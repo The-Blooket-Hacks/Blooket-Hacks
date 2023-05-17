5114887d7cb24e28c36383e63fdf9ab597241583
CrazyH <87752773+CrazyH2@users.noreply.github.com>
2023-05-17 09:10:51 +0100

    Update Flood Game.js

--- Global/Flood Game.js
+++ Global/Flood Game.js
@@ -4,11 +4,11 @@ if (console_msg("Flood Game")) {
 
 //new blooket api has rate limit so it's hard to make a flood thing.
 function getStateNode() {
-	for (let i of Object.keys(document.querySelector("#app>div>div"))) {
+	for (let i of Object.keys(document.querySelector("#app2 > div > div"))) {
 		if (i.toString().includes("__reactEventHandlers")) {
 			for (let p of Object.values(
 				document
-					.querySelector("#app>div>div")
+					.querySelector("#app2 > div > div")
 					[i].children.filter((n) => n)
 			)) {
 				if (p._owner && p._owner.stateNode) return p._owner.stateNode;
