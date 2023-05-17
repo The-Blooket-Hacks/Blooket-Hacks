a6a361284b17f16cce405fe32dc1d185a36990f8
CrazyH <87752773+CrazyH2@users.noreply.github.com>
2023-05-17 09:09:16 +0100

    Update All Answers Correct.js

--- Global/All Answers Correct.js
+++ Global/All Answers Correct.js
@@ -2,7 +2,7 @@
 
 if (console_msg("All Answers Correct")) {
 setInterval(function() {
-  var obj = Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.question;
+  var obj = Object.values(document.querySelector("#app2 > div > div"))[1].children[0]._owner.stateNode.state.question;
   try{
    obj.correctAnswers = obj.answers;
   }catch(genericError) {}
