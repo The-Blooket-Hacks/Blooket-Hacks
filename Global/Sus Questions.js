182ca7504a46dbca6eedeacf7ae01f39c74a10d4
CrazyH <87752773+CrazyH2@users.noreply.github.com>
2023-05-19 10:56:44 +0100

    Update Sus Questions.js

--- Global/Sus Questions.js
+++ Global/Sus Questions.js
@@ -2,11 +2,11 @@
 
 if (console_msg("Sus Questions")) {
 function makeQuestionSus() {
-Object.values(document.querySelector("#app2 > div > div"))[1].children[0]._owner.stateNode.state.question.answers = ["An Impostor", "An Impostor", "An Impostor", "An Impostor"];
-Object.values(document.querySelector("#app2 > div > div"))[1].children[0]._owner.stateNode.state.question.correctAnswers = ["An Impostor", "An Impostor", "An Impostor", "An Impostor"];
-Object.values(document.querySelector("#app2 > div > div"))[1].children[0]._owner.stateNode.state.question.text = "What is this?";
-Object.values(document.querySelector("#app2 > div > div"))[1].children[0]._owner.stateNode.state.question.image = "https://media.blooket.com/image/upload/v1671304785/enqwowk4ubpdgyztjbmt.png";
-Object.values(document.querySelector("#app2 > div > div"))[1].children[0]._owner.stateNode.forceUpdate()
+Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.question.answers = ["An Impostor", "An Impostor", "An Impostor", "An Impostor"];
+Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.question.correctAnswers = ["An Impostor", "An Impostor", "An Impostor", "An Impostor"];
+Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.question.text = "What is this?";
+Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.question.image = "https://media.blooket.com/image/upload/v1671304785/enqwowk4ubpdgyztjbmt.png";
+Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.forceUpdate()
 }
 
 window.setInterval(makeQuestionSus)
