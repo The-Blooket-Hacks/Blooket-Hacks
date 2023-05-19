0509c653e3911aebba0baed5d53b82227df58ba9
CrazyH <87752773+CrazyH2@users.noreply.github.com>
2023-05-19 11:01:34 +0100

    Update Disable glitches.js

--- Factory/Disable glitches.js
+++ Factory/Disable glitches.js
@@ -2,9 +2,9 @@
 
 if (console_msg("Disable Glitches")) {
 function stopGlitch() {
-Object.values(document.querySelector("#app2 > div > div"))[1].children[0]._owner.stateNode.state.joke = false
-Object.values(document.querySelector("#app2 > div > div"))[1].children[0]._owner.stateNode.state.dance = false
-Object.values(document.querySelector("#app2 > div > div"))[1].children[0]._owner.stateNode.state.lol = false
+Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.joke = false
+Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.dance = false
+Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.lol = false
 }
 
 window.setInterval(stopGlitch);
