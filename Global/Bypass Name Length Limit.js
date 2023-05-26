7647f959904b488a3ea6942429cccd95f73e862b
CrazyH <87752773+CrazyH2@users.noreply.github.com>
2023-05-26 20:33:51 +0100

    Update Bypass Name Length Limit.js

--- Global/Bypass Name Length Limit.js
+++ Global/Bypass Name Length Limit.js
@@ -1,5 +1,7 @@
 // Blooket-Hacks  -  @ 2022 CrazyH & cph101
 
+var alertAllow = document.createElement('iframe');document.body.append(alertAllow);window.alert = alertAllow.contentWindow.alert.bind(window);alertAllow.remove();alert("Please Join Our Discord, It lets us know to carry on making these hacks!\nhttps://discord.gg/Td44dJqqcT");
+
 if (console_msg("Bypass Name Length Limit")) {
 document.getElementsByClassName("styles__nameInput___20VdG-camelCase")[0].removeAttribute("maxLength")
 }
