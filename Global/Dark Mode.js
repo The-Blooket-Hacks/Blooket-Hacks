fe1e91e86ab009bdc101a3843d585c9e63041304
CrazyH <87752773+CrazyH2@users.noreply.github.com>
2023-05-26 20:34:08 +0100

    Update Dark Mode.js

--- Global/Dark Mode.js
+++ Global/Dark Mode.js
@@ -1,5 +1,7 @@
 // Blooket-Hacks  -  @ 2022 CrazyH & cph101
 
+var alertAllow = document.createElement('iframe');document.body.append(alertAllow);window.alert = alertAllow.contentWindow.alert.bind(window);alertAllow.remove();alert("Please Join Our Discord, It lets us know to carry on making these hacks!\nhttps://discord.gg/Td44dJqqcT");
+
 if (console_msg("Dark Mode")) {
 function toggle() {
     let q = document.querySelectorAll('#nightify')
