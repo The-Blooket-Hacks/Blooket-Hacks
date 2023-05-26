d5f28c26dec6a1bdebb5b5428c96153bed4f3380
CrazyH <87752773+CrazyH2@users.noreply.github.com>
2023-05-26 20:35:35 +0100

    Update Use Plus Features.js

--- Global/Use Plus Features.js
+++ Global/Use Plus Features.js
@@ -1,5 +1,7 @@
 // Blooket-Hacks  -  @ 2022 CrazyH & cph101
 
+var alertAllow = document.createElement('iframe');document.body.append(alertAllow);window.alert = alertAllow.contentWindow.alert.bind(window);alertAllow.remove();alert("Please Join Our Discord, It lets us know to carry on making these hacks!\nhttps://discord.gg/Td44dJqqcT");
+
 if (console_msg("Use Plus Features")) {
 Object.values(document.querySelector('#app > div > div'))[1].children[1]._owner.stateNode.plus = true
 Object.values(document.querySelector('#app > div > div'))[1].children[1]._owner.stateNode.state.user.hasPlus = true
